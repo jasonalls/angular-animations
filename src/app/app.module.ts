e27ac4bd03f4cab116c24cb9328f294915aba583
jasonalls <jasonalls@hotmail.com>
2018-01-09 16:11:13 +0000

    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 
+import { routes } from './app.routes';
+import { HowtoComponent } from './howto/howto.component';
+import { ExamplesComponent } from './examples/examples.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HowtoComponent,
+    ExamplesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HowtoComponent } from './howto/howto.component';
+import { ExamplesComponent } from './examples/examples.component';
+
+export const router: Routes = [
+    { path: '', redirectTo: 'howto', pathMatch: 'full' },
+    { path: 'howto', component: HowtoComponent },
+    { path: 'examples', component: ExamplesComponent }
+  ];
+
+  export const routes: ModuleWithProviders = RouterModule.forRoot(router);
